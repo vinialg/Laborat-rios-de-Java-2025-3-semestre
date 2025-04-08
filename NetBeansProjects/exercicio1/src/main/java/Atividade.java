b2a8375ab62bece3c1564bf71ebffcb9221e98fe
VINICIUS ALVES GIMENEZ <unievgimenez@feinet.fei.edu.br>
2025-04-08 20:18:22 -0300

    Laboratorio 1

---
+++ NetBeansProjects/exercicio1/src/main/java/Atividade.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author unievgimenez
+ */
+//import.java.util.Scanner;;
+//Exercicio 1
+public class Atividade {
+    public static void main(String[] args){
+        //Scanner input = new Scanner(System.in);
+        
+        for (int par = 0; par <= 100; par = par + 2){
+                System.out.printf("%d\n", par);
+        }
+    }
+}
